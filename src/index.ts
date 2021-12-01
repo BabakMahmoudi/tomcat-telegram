dd3ee068fac4eac6e8ed017141aa002badcec948
pariamahmoudi <paria.mahmoudi@gmail.com>
2021-12-01 16:19:11 +0000

    first try

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,68 @@
-// const TelegramBot = require('node-telegram-bot-api');
+import tomcat from '@gostarehnegar/tomcat'
 import TelegramBot from 'node-telegram-bot-api'
+tomcat.config.data.redis.publicUrl = "redis://localhost:6379";
+tomcat.config.messaging.transports.websocket.diabled = true;
+const bus = new tomcat.Infrastructure.Bus.RedisBus();
 
-// replace the value below with the Telegram token you receive from @BotFather
+
 const token = '2140430977:AAHd0qR1mUiQdxXKgG_8RfgJ7oR63JJSDqs';
+const bot = new TelegramBot(token, { polling: true },);
+bus.publish("bots/telegram/controls/started", { port: null, id: "telegram" })
 
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, { polling: true });
+class Subscriptions {
+    public items: { chatId: string, sub: string }[] = []
+    getSubscriptions(id: string) {
+        const subs = []
+        this.items.forEach((x) => {
+            if (x.chatId == id) {
+                subs.push(x.sub)
+            }
+        })
+        return subs
+    }
+}
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
+bot.onText(/\/subscribe/, (msg) => {
 
-    const chatId = msg.chat.id;
-    const resp = match[1]; // the captured "whatever"
+    bot.sendMessage(msg.chat.id, "which?", {
+        "reply_markup": {
+            "keyboard": [["mohsen"]]
+        }
+    });
 
-    // send back the matched "whatever" to the chat
-    bot.sendMessage(chatId, resp);
 });
+bot.onText(/\/unsubscribe/, (msg) => {
+    const sub = subs.getSubscriptions(msg.chat.id)
+    bot.sendMessage(msg.chat.id, "which one?", {
+        "reply_markup": {
+            "keyboard": [sub]
+        }
+    });
 
-// Listen for any kind of message. There are different kinds of
-// messages.
+});
+const subs = new Subscriptions()
 bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
+    if (msg.text.indexOf("mohsen") === 0) {
+        const a = subs.getSubscriptions(msg.chat.id)
+        if (a.indexOf("mohsen") < 0) {
 
-    // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, msg.text);
+            bus.subscribe("bots/mohsen/controls/*", (m) => {
+                const message = m.content<{ port: number, id: string }>();
+                bot.sendMessage(msg.chat.id, `<a href="http://127.0.0.1:${message.port}/trades">Trades</a>`, { parse_mode: "HTML" });
+            })
+            bus.subscribe("bots/mohsen/signals/*", (m) => {
+                const message = m.content<{ signal: string, candle: tomcat.Domain.Base.CandleStickData }>();
+                bot.sendMessage(msg.chat.id, `${message.signal} recevied at ${message.candle.closeTime} price : ${message.candle.close}`);
+
+            })
+            bot.sendMessage(msg.chat.id, "subscribed to mohsen");
+            subs.items.push({ chatId: msg.chat.id, sub: "mohsen" })
+        } else {
+
+            bot.sendMessage(msg.chat.id, "already subscribed to mohsen");
+        }
+    }
 });
+bot.on('error', (err) => {
+    console.log(err);
+})
