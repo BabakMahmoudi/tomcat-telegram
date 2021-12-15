168d068858ac729c42e2755ecfe43a4b151adf29
pariamahmoudi <paria.mahmoudi@gmail.com>
2021-12-15 11:41:19 +0000

    minor changes

--- src/index.ts
+++ src/index.ts
@@ -45,20 +45,21 @@ const subscribed = false
 bot.onText(/\/start/, (msg) => {
     if (!subscribed) {
         redisBus.subscribe("bots/mohsen/controls/*", (m) => {
-                const message = m.content<{ port: number, id: string }>();
-                bot.sendMessage(msg.chat.id, `<a href="http://127.0.0.1:${message.port}/trades">Trades</a>`, { parse_mode: "HTML" });
-            })
+            const message = m.content<{ port: number, id: string }>();
+            bot.sendMessage(msg.chat.id, `<a href="http://127.0.0.1:${message.port}/trades">Trades</a>`, { parse_mode: "HTML" });
+        })
         redisBus.subscribe("bots/mohsen/wallet/*", (m) => {
-            const message = m.content<{ doge: number, usdt: number, time: string }>()
             if (m.Topic.includes("buy")) {
+                const message = m.content<{ usdt: number, time: string }>()
                 bot.sendMessage(msg.chat.id, `bought ${message.usdt} worth of doge at ${message.time}`)
             }
             else if (m.Topic.includes("sell")) {
+                const message = m.content<{ doge: number, time: string }>()
                 bot.sendMessage(msg.chat.id, `sold ${message.doge} DOGE at ${message.time}`)
             }
         })
-        bot.sendMessage(msg.chat.id , "subscribed")
-    }else{
+        bot.sendMessage(msg.chat.id, "subscribed")
+    } else {
 
         bot.sendMessage(msg.chat.id, "already subscribed")
     }
